b8bea0162ed179c34dd8e1663fe39300f369da7c
baumannr <baumann.robert95@gmail.com>
2025-09-03 16:44:15 +0200

    add initial project

---
+++ src/main/java/hu/baumannr/personregistryapi/PersonRegistryApiApplication.java
@@ -0,0 +1,13 @@
+package hu.baumannr.personregistryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonRegistryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonRegistryApiApplication.class, args);
+	}
+
+}
