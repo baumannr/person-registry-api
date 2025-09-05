883cb42ad87c93dc420d8f5eb1e885f66022c31e
baumannr <baumann.robert95@gmail.com>
2025-09-05 19:42:49 +0200

    add exception handling for no content found and not supported method

--- src/main/java/hu/baumannr/personregistryapi/exception/PersonRegistryApiExceptionHandler.java
+++ src/main/java/hu/baumannr/personregistryapi/exception/PersonRegistryApiExceptionHandler.java
@@ -6,8 +6,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.time.LocalDateTime;
 
@@ -31,13 +33,47 @@ public class PersonRegistryApiExceptionHandler {
                 .body(response);
     }
 
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNoHandlerFoundException(NoHandlerFoundException exception) {
+        String requestURL = exception.getRequestURL();
+        String httpMethod = exception.getHttpMethod();
+        String path = String.join(" ", httpMethod, requestURL);
+
+        log.debug("No handler found for path: {}", path);
+
+        ErrorResponse response = new ErrorResponse()
+                .timestamp(LocalDateTime.now())
+                .error(HttpStatus.NOT_FOUND.getReasonPhrase())
+                .message("No endpoint available for: " + path);
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(response);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleHttpRequestMethodNotSupportedException(
+            HttpRequestMethodNotSupportedException exception) {
+        log.debug(exception.getMessage());
+
+        ErrorResponse response = new ErrorResponse()
+                .timestamp(LocalDateTime.now())
+                .error(HttpStatus.METHOD_NOT_ALLOWED.getReasonPhrase())
+                .message(exception.getMessage());
+
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+                .body(response);
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneralException(Exception exception) {
         log.error("Unexpected error", exception);
+
         ErrorResponse response = new ErrorResponse()
                 .timestamp(LocalDateTime.now())
                 .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
                 .message(UNEXPECTED_ERROR);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
     }
 }
