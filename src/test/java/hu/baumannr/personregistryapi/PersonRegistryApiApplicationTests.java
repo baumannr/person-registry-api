36a468fd7a7f7d346236b9d397139f4e144222b0
baumannr <baumann.robert95@gmail.com>
2025-09-05 10:16:58 +0200

    add application-test.properties, add schema.sql, set active profile for spring boot test

--- src/test/java/hu/baumannr/personregistryapi/PersonRegistryApiApplicationTests.java
+++ src/test/java/hu/baumannr/personregistryapi/PersonRegistryApiApplicationTests.java
@@ -2,8 +2,10 @@ package hu.baumannr.personregistryapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PersonRegistryApiApplicationTests {
 
 	@Test
