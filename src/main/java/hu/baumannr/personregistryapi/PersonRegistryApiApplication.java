3a97154566c8ea30c2d16519e6c51bdc7fa9f530
baumannr <baumann.robert95@gmail.com>
2025-09-05 18:47:57 +0200

    enable JPA repositories

--- src/main/java/hu/baumannr/personregistryapi/PersonRegistryApiApplication.java
+++ src/main/java/hu/baumannr/personregistryapi/PersonRegistryApiApplication.java
@@ -2,8 +2,10 @@ package hu.baumannr.personregistryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class PersonRegistryApiApplication {
 
 	public static void main(String[] args) {
